331046fd5c108258509f57c0a276006a32bd0103
jashanpreet kaur <jashankhaire55@gmail.com>
2025-02-25 17:57:39 +0530

    initial-commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // 🔹 Disable "any" rule
+      "@typescript-eslint/no-unused-vars": "warn", // 🔹 Show warnings instead of errors
+      "react/no-unescaped-entities": "off", // 🔹 Disable unescaped apostrophe error
+    },
+  },
 ];
 
 export default eslintConfig;
